e7b7b7875ffa2ec1d4d595f081651f39a03042fa
Manasvi Patel <132248751+Manasvi2805@users.noreply.github.com>
2023-05-01 01:35:07 -0400

    Create StudentIDCard.py

---
+++ StudentIDCard.py
@@ -0,0 +1,18 @@
+F_name = input("Enter First Name : ")
+L_name = input("Enter Last Name : ")
+Age = int(input("Enter Age : "))
+ph_no = input("Enter Phone Number : ")
+Blood_Group = input("Enter Blood Group : ")
+Semester = int(input("Enter Semester : "))
+
+print("-"*40)
+print("Student ID Card")
+print("-"*40)
+
+print("Student First Name : ",F_name)
+print("Student Last Name : ",L_name)
+print("Student Age : ",Age)
+print("Student Phone Number : ",ph_no)
+print("Student Blood Group : ",Blood_Group)
+print("Student Semester : ",Semester)
+
