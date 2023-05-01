24b7f666ccedf74abf0e0937cdf9beb602a560c5
Manasvi Patel <132248751+Manasvi2805@users.noreply.github.com>
2023-05-01 02:12:41 -0400

    Create Swap2VarValue.py

---
+++ Swap2VarValue.py
@@ -0,0 +1,17 @@
+# Write a Program to swap 2 variable value.
+a = 56
+b = 45
+print(a,b)
+a,b = b,a
+print (a,b)
+
+
+#  Write a Program to swap 2 variable value using temp variable.
+
+a = 50
+b = 60
+temp = a
+print(a,b)
+a = b
+b = temp 
+print(a,b)
